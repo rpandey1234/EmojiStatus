b416d26d9a792b77c6169b19ef20bc52d20ade6c
Rahul Pandey <hello@rkpandey.com>
2020-08-23 15:17:28 -0700

    Add logout option on main menu

--- app/src/main/java/edu/stanford/rkpandey/emojistatus/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/emojistatus/MainActivity.kt
@@ -1,11 +1,42 @@
 package edu.stanford.rkpandey.emojistatus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    
+    private companion object {
+        private const val TAG = "MainActivity"
+    }
+    
+    private lateinit var auth: FirebaseAuth
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.miLogout) {
+            Log.i(TAG, "Logout")
+            auth.signOut()
+            val logoutIntent = Intent(this, LoginActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logoutIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
