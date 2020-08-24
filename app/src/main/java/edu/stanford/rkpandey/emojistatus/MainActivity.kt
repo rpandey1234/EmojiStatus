2e628e1ea89b3ea2ba95a7ba9ffd295507f38016
Rahul Pandey <hello@rkpandey.com>
2020-08-23 17:18:17 -0700

    Add FirestoreRecyclerAdapter to show users and emojis in RV

--- app/src/main/java/edu/stanford/rkpandey/emojistatus/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/emojistatus/MainActivity.kt
@@ -1,27 +1,62 @@
 package edu.stanford.rkpandey.emojistatus
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+
+data class User(
+    val displayName: String = "",
+    val emojis: String = ""
+)
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
 class MainActivity : AppCompatActivity() {
     
     private companion object {
         private const val TAG = "MainActivity"
     }
-    
+
+    private val db = Firebase.firestore
     private lateinit var auth: FirebaseAuth
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = Firebase.auth
+
+        // Query the users collection
+        val query = db.collection("users")
+        val options = FirestoreRecyclerOptions.Builder<User>().setQuery(query, User::class.java)
+            .setLifecycleOwner(this).build()
+        val adapter = object: FirestoreRecyclerAdapter<User, UserViewHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+                val view = LayoutInflater.from(this@MainActivity).inflate(android.R.layout.simple_list_item_2, parent, false)
+                return UserViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: UserViewHolder, position: Int, model: User) {
+                val tvName: TextView = holder.itemView.findViewById(android.R.id.text1)
+                val tvEmojis: TextView = holder.itemView.findViewById(android.R.id.text2)
+                tvName.text = model.displayName
+                tvEmojis.text = model.emojis
+            }
+        }
+        rvUsers.adapter = adapter
+        rvUsers.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
